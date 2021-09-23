f8297aa47e1621393e8ba699d67a6c851bc28b65
Harry <hdentry56@gmail.com>
2021-09-23 15:59:03 +1000

    invite command

--- Bot.py
+++ Bot.py
@@ -56,6 +56,30 @@ async def on_message(message: discord.Message):
         elif root == "prefix":
             prefix = msg[(len(prefix) + len("prefix ")):]
             await message.channel.send("Set prefix to: '" + prefix + "'")
+        elif root == "invite":
+            # Get game
+            if not message.author in starting_games.keys():
+                await message.channel.send("No Game Found. Create a game with the " + prefix + "game command first")
+                return
+            game: Game.Game = starting_games[message.author]
+
+            # TODO: Check if game has max players
+
+            # Get User
+            user = await get_user(ctx, command, 1)
+            if user == None:
+                await message.channel.send(prefix + "invite @user")
+                return
+
+            # TODO: Check if game can accept this player
+            # if not game.validate_player(user):
+            #     await message.channel.send(user.name + " cannot be added to this game")
+
+            # Add user to players list TODO: Let user accept invite?
+            game.players.append(user)
+            await message.channel.send("Added " + user.name + " to the game")
+
+
         elif root == "play":
             if not message.author in starting_games.keys():
                 await message.channel.send("No Game Found. Create a game with the " + prefix + "game command first")
@@ -77,6 +101,12 @@ async def start_game(ctx, channel, name, user) -> bool:
             starting_games[user] = await game.create(ctx, channel, user)
             return True
     return False
+
+async def get_user(ctx: discord.Client, command: list, index: int) -> discord.User:
+    '''Returns a user from a mention within a command, formated string <@userid>'''
+    if len(command) > 1 and len(command[index]) > 3 and command[index][:2] == "<@" and command[index][-1:] == ">" and command[index][3] != "!":
+        return await ctx.fetch_user(int(command[index][2:-1]))
+    return None
 
 def is_match(input: str, alias: list()):
     return input.lower() in (s.lower() for s in alias)
